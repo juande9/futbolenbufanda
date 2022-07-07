dbb8c10f6dc3d035d2bd4e5a049b7791cb4f0b5a
Juan Manuel <juanmavazquez91@gmail.com>
2022-07-07 00:39:42 -0300

    instalacion sass. cosas pulidas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map = L.map('map').
     setView([20, 0],
-        2);
+        3);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
