fb17cbc2a2b420f5cdbb0d3b2f792c4d581293dc
Juan Manuel <juanmavazquez91@gmail.com>
2022-07-29 15:56:38 -0300

    29/7

--- scriptmain.js
+++ scriptmain.js
@@ -1,24 +1,27 @@
 const ligas = ["LaLiga", "Primera División RFEF", "Serie A", "Ligue 1", "Premier League", "Primeira Liga Portugal", "Primera División de Argentina", "Serie B","Eredivisie"]
 const paises = ["Argentina", "Italia", "España", "Inglaterra", "Francia", "Portugal", "Paises Bajos"]
 const decada = ["80s", "90s", "00s", "10s", "20s"]
-const equipos = []
 const bufandas = []
 
-/* Equipos Españoles*/
 
-const realMadrid = new Equipo("Real Madrid", ligas[0], paises[2], "realMadrid", "Santiago Bernabéu", "40.45306", "-3.68835")
-const barcelona = new Equipo("Barcelona", ligas[0], paises[2], "barcelona", "Camp Nou", "41.380833", "2.1225")
-const rcCelta = new Equipo("RC Celta de Vigo", ligas[0], paises[2], "celtaVigo", "Estadio de Balaídos", "42.211861", "-8.740328")
-const deporCoruña = new Equipo("RC Deportivo de La Coruña",ligas[1],paises[2],"deporCoruna","Estadio de Riazor","43.368667","-8.417372")
-const sevilla = new Equipo("Sevilla Fútbol Club",ligas[0],paises[2],"sevilla","Estadio Ramón Sánchez-Pizjuán","37.383878","-5.970467")
+const eqEspañoles=[
+new Equipo("Real Madrid", ligas[0], paises[2], "realMadrid", "Santiago Bernabéu", "40.45306", "-3.68835"),
+new Equipo("Barcelona", ligas[0], paises[2], "barcelona", "Camp Nou", "41.380833", "2.1225"),
+new Equipo("RC Celta de Vigo", ligas[0], paises[2], "celtaVigo", "Estadio de Balaídos", "42.211861", "-8.740328"),
+new Equipo("RC Deportivo de La Coruña",ligas[1],paises[2],"deporCoruna","Estadio de Riazor","43.368667","-8.417372"),
+new Equipo("Sevilla Fútbol Club",ligas[0],paises[2],"sevilla","Estadio Ramón Sánchez-Pizjuán","37.383878","-5.970467"),
+]
 
-/* Argentinos */
-const independiente = new Equipo("C.A. Independiente", ligas[6], paises[0], "independiente", "Estadío Libertadores de America", "-34.670267", "-58.370969")
+const eqArgentina=[
+new Equipo("C.A. Independiente", ligas[6], paises[0], "independiente", "Estadío Libertadores de America", "-34.670267", "-58.370969")
+]
 
-/*Paises Bajos*/
-const ajax = new Equipo("AFC Ajax",ligas[8],paises[6],"ajax","Johan Cruyff Arena","52.314167","4.941944")
+const eqPaisesBajos=[
+new Equipo("AFC Ajax",ligas[8],paises[6],"ajax","Johan Cruyff Arena","52.314167","4.941944"),
+]
 
-equipos.push(realMadrid, barcelona, rcCelta, independiente,ajax,deporCoruña,sevilla)
+const equipos = eqEspañoles.concat(eqArgentina,eqPaisesBajos)
+
 
 /**
  * Sirve para agregar equipos al array equipos
@@ -37,6 +40,6 @@ function agregarEquipo(Arrayequipos) {
     Arrayequipos.push(equipo)
 }
 
-const bufMadrid1 = new Bufanda(realMadrid.nombre, decada[2], "./img/buf/madrid1.png")
+/* const bufMadrid1 = new Bufanda(realMadrid.nombre, decada[2], "./img/buf/madrid1.png")
 const bufBarca2 = new Bufanda(barcelona.nombre, decada[1], "./img/buf/barca1.png")
-bufandas.push(bufMadrid1, bufBarca2)
+bufandas.push(bufMadrid1, bufBarca2) */
--- scriptmap.js
+++ scriptmap.js
@@ -1,6 +1,27 @@
 var map = L.map('map').
     setView([20, 0],
         3);
+
+var CoordPais = [españa]
+
+var españa = L.polygon([
+    [43.610, -8.591], [42.476, 2.993], [39.801, 4.404], [36.686, -2.124], [36.073, -5.472], [37.188, -7.353], [41.901, -6.226],[42.011, -8.794],
+]).addTo(map);
+
+CoordPais.forEach(pais =>
+    map.on("click", (pais1) => {
+        map.fitBounds(pais1.getBounds()),
+            currentZoom = 5,
+            checkEscudos(currentZoom)
+    }))
+
+/* map.on("click", () => {
+    map.fitBounds(españa.getBounds()),
+        currentZoom = 5,
+        checkEscudos(currentZoom)
+}) */
+
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 10,
     minZoom: 2,
@@ -15,16 +36,33 @@ let mapa = document.getElementById("map")
 mapa.onwheel = (e) => {
     e.preventDefault()
     currentZoom += e.deltaY * -0.01;
+    currentZoom = Math.min(Math.max(2, currentZoom), 9)
     checkEscudos(currentZoom);
 }
 
-for (let i = 0; i < equipos.length; i++) {
+equipos.forEach(elemento => {
     var myIcon = L.icon({
-        iconUrl: equipos[i].escudo,
+        iconUrl: elemento.escudo,
         iconAnchor: [5, 5],
         className: "escudosMapa"
     });
-    var marker = L.marker([equipos[i].coordenadlat, equipos[i].coordenadlong], { icon: myIcon }).addTo(map);
-}
+    L.marker([elemento.coordenadlat, elemento.coordenadlong], { icon: myIcon }).addTo(map);
+})
 
 var escudos = document.getElementsByClassName("escudosMapa")
+
+function checkEscudos(zoom) {
+    if (zoom <= 4) {
+        for (const logo of escudos) {
+            logo.style.display = 'none'
+        }
+    } else if (zoom === 5) {
+        for (const logo of escudos) {
+            logo.style.display = 'block'
+            logo.style.height = "2.5em"
+        }
+    } else if (zoom === 6)
+        for (const logo of escudos) {
+            logo.style.height = '3.5em'
+        }
+}
