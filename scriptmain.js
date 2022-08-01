3d71f7533b1f9f41324f3395793861d8973ca35d
Juan Manuel <juanmavazquez91@gmail.com>
2022-08-01 00:48:03 -0300

    ultimo merge

--- scriptmain.js
+++ scriptmain.js
@@ -4,7 +4,6 @@ const decada = ["80s", "90s", "00s", "10s", "20s"]
 const bufandas = []
 
 
-<<<<<<< HEAD
 const eqEspañoles = [
     new Equipo("Real Madrid", ligas[0], paises[2], "realMadrid", "Santiago Bernabéu", "40.45306", "-3.68835"),
     new Equipo("Barcelona", ligas[0], paises[2], "barcelona", "Camp Nou", "41.380833", "2.1225"),
@@ -35,25 +34,6 @@ seleccionPais = ""
 paises.forEach(element =>
     seleccionPais += `<option value=${element.replace(/ /g, "")}>${element}</option>`)
 selectorPais.innerHTML = seleccionPais
-=======
-const eqEspañoles=[
-new Equipo("Real Madrid", ligas[0], paises[2], "realMadrid", "Santiago Bernabéu", "40.45306", "-3.68835"),
-new Equipo("Barcelona", ligas[0], paises[2], "barcelona", "Camp Nou", "41.380833", "2.1225"),
-new Equipo("RC Celta de Vigo", ligas[0], paises[2], "celtaVigo", "Estadio de Balaídos", "42.211861", "-8.740328"),
-new Equipo("RC Deportivo de La Coruña",ligas[1],paises[2],"deporCoruna","Estadio de Riazor","43.368667","-8.417372"),
-new Equipo("Sevilla Fútbol Club",ligas[0],paises[2],"sevilla","Estadio Ramón Sánchez-Pizjuán","37.383878","-5.970467"),
-]
-
-const eqArgentina=[
-new Equipo("C.A. Independiente", ligas[6], paises[0], "independiente", "Estadío Libertadores de America", "-34.670267", "-58.370969")
-]
-
-const eqPaisesBajos=[
-new Equipo("AFC Ajax",ligas[8],paises[6],"ajax","Johan Cruyff Arena","52.314167","4.941944"),
-]
-
-const equipos = eqEspañoles.concat(eqArgentina,eqPaisesBajos)
->>>>>>> fb17cbc2a2b420f5cdbb0d3b2f792c4d581293dc
 
 
 /**
